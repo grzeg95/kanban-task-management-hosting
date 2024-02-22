c24ad956bcb203dd64aa06b97e1e7c0eda2e4042
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2024-02-22 11:49:56 +0100

    feat: add theme-selector.service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import {ApplicationConfig} from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
+import {ThemeSelectorService} from './services/theme-selector.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(),
+    ThemeSelectorService,
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [ThemeSelectorService],
+      useFactory: (themeSelectorService: ThemeSelectorService) => {
+        return () => themeSelectorService.loadTheme();
+      }
+    }
   ]
 };
---
+++ src/app/services/theme-selector.service.ts
@@ -0,0 +1,91 @@
+import {DOCUMENT, isPlatformBrowser} from '@angular/common';
+import {Inject, Injectable, PLATFORM_ID, Renderer2, RendererFactory2, signal} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeSelectorService {
+
+  private readonly _renderer: Renderer2;
+  readonly isDark = signal<boolean | undefined>(undefined);
+
+  constructor(
+    private readonly _rendererFactory: RendererFactory2,
+    @Inject(DOCUMENT) private readonly _document: Document,
+    @Inject(PLATFORM_ID) private readonly _platformId: NonNullable<unknown>
+  ) {
+    this._renderer = this._rendererFactory.createRenderer(null, null);
+  }
+
+  setLight() {
+
+    const isBrowser = isPlatformBrowser(this._platformId);
+
+    this._renderer.removeClass(this._document.body, 'dark');
+    this._renderer.addClass(this._document.body, 'light');
+
+    if (isBrowser) {
+      localStorage.setItem('theme', 'light');
+    }
+
+    this.isDark.set(false);
+  }
+
+  setDark() {
+
+    const isBrowser = isPlatformBrowser(this._platformId);
+
+    this._renderer.removeClass(this._document.body, 'light');
+    this._renderer.addClass(this._document.body, 'dark');
+
+    if (isBrowser) {
+      localStorage.setItem('theme', 'dark');
+    }
+
+    this.isDark.set(true);
+  }
+
+  loadTheme() {
+
+    const isBrowser = isPlatformBrowser(this._platformId);
+
+    if (isBrowser) {
+
+      const storedTheme = localStorage.getItem('theme');
+
+      if (storedTheme === 'light' || storedTheme === 'dark') {
+
+        switch (storedTheme) {
+          case 'light':
+            this.setLight();
+            break;
+          case 'dark':
+            this.setDark();
+            break;
+        }
+
+      } else {
+
+        let theme: 'dark' | 'light' = 'dark';
+
+        const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches;
+
+        if (!isLightMode) {
+          theme = 'light';
+        }
+
+        switch (theme) {
+          case 'light':
+            this.setLight();
+            break;
+          case 'dark':
+            this.setDark();
+            break;
+        }
+      }
+
+    } else {
+      this.setDark();
+    }
+  }
+}
